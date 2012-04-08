88eca9e7790a3c2f3ba0ddb67df0b80b2028f136
ArnoldZokas <arnold.zokas@coderoom.net>
2012-04-08 23:37:27 +0100

    Renamed project MvcNavigation.IntegrationTests to MvcNavigation.DisplayTemplates

--- source/MvcNavigation.IntegrationTests/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// # Copyright © 2012, Arnold Zokas
-// # All rights reserved. 
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("MvcNavigation.IntegrationTests")]
-[assembly: AssemblyDescription("MvcNavigation.IntegrationTests")]
