043ba8e206d6d38e674acd11c5999a5ed3e1b566
milosc <milos.cigoj@gmail.com>
2015-09-23 22:00:42 +0200

    Introducing Links with Icon

--- source/MvcNavigation/Node.cs
+++ source/MvcNavigation/Node.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Web.Mvc;
@@ -9,19 +9,21 @@ namespace MvcNavigation
 	{
 		IList<INode> _childNodes;
 
-		public Node(Expression<Action<TController>> action) : this(action, null, new INode[0])
+        public Node(Expression<Action<TController>> action)
+            : this(action, null, String.Empty, new INode[0])
 		{
 		}
 
-		public Node(Expression<Action<TController>> action, string title) : this(action, title, new INode[0])
+		public Node(Expression<Action<TController>> action, string title) : this(action, title, String.Empty, new INode[0])
 		{
 		}
 
-		public Node(Expression<Action<TController>> action, params INode[] childNodes) : this(action, null, childNodes)
+        public Node(Expression<Action<TController>> action, params INode[] childNodes)
+            : this(action, null, String.Empty, childNodes)
 		{
 		}
 
-		public Node(Expression<Action<TController>> action, string title, params INode[] childNodes)
+        public Node(Expression<Action<TController>> action, string title, string iconFileName, params INode[] childNodes)
 		{
 			if (action == null)
 				throw new ArgumentNullException("action");
@@ -33,7 +35,7 @@ namespace MvcNavigation
 			if (methodCallExpression == null)
 				throw new ArgumentException("Node must be initialised with method call expression (e.g. controller => controller.Action())", "action");
 
-			Initialise(methodCallExpression, title);
+			Initialise(methodCallExpression, title, iconFileName);
 			InitialiseChildNodes(childNodes);
 		}
 
@@ -61,16 +63,30 @@ namespace MvcNavigation
 		{
 		}
 
+        public Node(Expression<Action<TController>> action, string title, string iconFileName)
+            : this(action, title, iconFileName, new INode[0])
+        {
+        }
+
 		public Node(Expression<Action<TController>> action, params INode[] childNodes) : this(action, null, childNodes)
 		{
 		}
 
-		public Node(Expression<Action<TController>> action, string title, params INode[] childNodes) : base(action, title, childNodes)
+		public Node(Expression<Action<TController>> action, string title,  params INode[] childNodes) : base(action, title, String.Empty, childNodes)
 		{
 			// ReSharper disable DoNotCallOverridableMethodsInConstructor
 			AreaName = Activator.CreateInstance<TAreaRegistration>().AreaName;
 			SetAreaName(RouteValues, AreaName);
 			// ReSharper restore DoNotCallOverridableMethodsInConstructor
 		}
+
+        public Node(Expression<Action<TController>> action, string title, string iconFileName, params INode[] childNodes)
+            : base(action, title, iconFileName, childNodes)
+        {
+            // ReSharper disable DoNotCallOverridableMethodsInConstructor
+            AreaName = Activator.CreateInstance<TAreaRegistration>().AreaName;
+            SetAreaName(RouteValues, AreaName);
+            // ReSharper restore DoNotCallOverridableMethodsInConstructor
+        }
 	}
 }
