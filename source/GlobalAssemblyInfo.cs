3993edd22adb0d1439292d30bbb86b4404bd4ceb
milosc <milos.cigoj@gmail.com>
2015-09-23 22:04:40 +0200

    Why not release it
    Tested it and I suggest to release it. Why haven't you done this yet?

--- source/GlobalAssemblyInfo.cs
+++ source/GlobalAssemblyInfo.cs
@@ -9,10 +9,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("RELEASE")]
 #endif
 
-[assembly: AssemblyCopyright("Copyright © Arnold Zokas 2012")]
+[assembly: AssemblyCopyright("Copyright Â© Arnold Zokas 2012")]
 [assembly: AssemblyProduct("MvcNavigation")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha6")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
